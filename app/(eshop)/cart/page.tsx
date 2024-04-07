ebf536b907e2639ec15d8997ecf6216ad9a6aa7f
Douglas Deodanes <deodanes.dbz@gmail.com>
2024-04-06 19:57:44 -0600

    Update page.tsx

--- app/(eshop)/cart/page.tsx
+++ app/(eshop)/cart/page.tsx
@@ -1,6 +1,7 @@
 import ProductInCart from "@/components/cart/productInCart";
 import SummaryCart from "@/components/cart/SummaryCart";
 
+
 const ProductCart = () => {
   return (
     <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:px-16">
